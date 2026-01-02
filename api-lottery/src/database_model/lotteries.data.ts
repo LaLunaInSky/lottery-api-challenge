b01ac83761b845d9a1eea02de97762c8d317f16e
LaLunaInSky <moonaflordapele@gmail.com>
2026-01-02 13:31:08 -0300

    update lotteries_data

--- api-lottery/src/database_model/lotteries.data.ts
+++ api-lottery/src/database_model/lotteries.data.ts
@@ -2,44 +2,61 @@ const DataLotteries = [
     {
         "id": 1,
         "name": "mega-sena",
-        "number_of_draw": 0,
-        "date_of_draw": "01-01-2001",
-        "winning_numbers_draw": [1, 2, 3, 4, 5, 6],
+        "number_of_draw": 2955,
+        "date_of_draw": "01-01-2026",
+        "winning_numbers_draw": [
+            9, 13, 21, 32, 33, 59
+        ],
     },
     {
         "id": 2,
         "name": "quina",
-        "number_of_draw": 0,
-        "date_of_draw": "01-01-2001",
-        "winning_numbers_draw": [1, 2, 3, 4, 5],
+        "number_of_draw": 6916,
+        "date_of_draw": "01-01-2026",
+        "winning_numbers_draw": [
+            8, 54, 58, 72, 76
+        ],
     },
     {
         "id": 3,
         "name": "lotofácil",
-        "number_of_draw": 0,
-        "date_of_draw": "01-01-2001",
-        "winning_numbers_draw": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        "number_of_draw": 3576,
+        "date_of_draw": "01-01-2026",
+        "winning_numbers_draw": [
+            1, 2, 3, 5, 7,
+            8, 10, 13, 16, 18,
+            19, 21, 22, 23, 25
+        ],
     },
     {
         "id": 4,
         "name": "lotomania",
-        "number_of_draw": 0,
-        "date_of_draw": "01-01-2001",
-        "winning_numbers_draw": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+        "number_of_draw": 2869,
+        "date_of_draw": "01-01-2026",
+        "winning_numbers_draw": [
+            1, 11, 18, 22, 27,
+            32, 36, 37, 44, 45,
+            47, 53, 66, 70, 77,
+            84, 86, 92, 93, 95
+        ],
     },
     {
         "id": 5,
         "name": "timemania",
-        "number_of_draw": 0,
-        "date_of_draw": "01-01-2001",
-        "winning_numbers_draw": [1, 2, 3, 4, 5, 6, 7],
+        "number_of_draw": 2337,
+        "date_of_draw": "30-12-2025",
+        "winning_numbers_draw": [
+            8, 29, 35, 43, 62, 67, 70
+        ],
     },
     {
         "id": 6,
         "name": "dia de sorte",
-        "number_of_draw": 0,
-        "date_of_draw": "01-01-2001",
-        "winning_numbers_draw": [1, 2, 3, 4, 5, 6, 7],
+        "number_of_draw": 1158,
+        "date_of_draw": "30-12-2025",
+        "winning_numbers_draw": [
+            6, 9, 14, 15, 26, 27, 30
+        ],
     },
 ];
 
--- api-lottery/src/functions/test.ts
+++ api-lottery/src/functions/test.ts
@@ -1,19 +1,31 @@
 import PullAllLoteries from "../../drizzle/cruds/pull_all_loteries.js";
-import AddNewLottery from "../../drizzle/cruds/add_new_lottery.js";
+import UpdateAnLotteryInformations from "../../drizzle/cruds/update_an_lottery_informations.js";
 import DataLotteries from "../database_model/lotteries.data.js";
-import DeleteAnLottery from "../../drizzle/cruds/delete_an_lottery.js";
-import PullAnLotteryByName from "../../drizzle/cruds/pull_an_lottery_by_name.js";
 import AddTheIninitalDataModel from "./add_the_initial_data_model.js";
 
 const Test = async () => {
     // Use only when you have no data!!!
     // await AddTheIninitalDataModel();
 
-    // console.log(
-    //     await PullAnLotteryByName(
-    //         "quin"
-    //     )
-    // );
+    // Use this when updating results data
+    // DataLotteries.map(
+    //     async (lottery) => {
+    //         const {
+    //             name,
+    //             number_of_draw,
+    //             date_of_draw,
+    //             winning_numbers_draw
+    //         } = lottery;
+
+    //         await UpdateAnLotteryInformations(
+    //             name,
+    //             number_of_draw,
+    //             date_of_draw,
+    //             winning_numbers_draw
+    //         );
+    //     }
+    // )
+    // console.log(await PullAllLoteries());
 };
 
 export default Test;
